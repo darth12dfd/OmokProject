5f6db8a7ebaed5cdd35a1b9a8bf1b75f92cf03de
darth12dfd <woni011420@gmail.com>
2024-05-14 15:17:13 +0900

    GUI 클래스, 메인 클래스의 Test Unit 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import client.Client;
+import server.Server;
+
 public class Main {
     public static void main(String[] args) {
+        // 서버 실행
+        Thread serverThread = new Thread(() -> {
+            Server server = new Server();
+            //server.start();
+        });
+        serverThread.start();
 
+        // 클라이언트 실행
+        Thread client1Thread = new Thread(() -> {
+            Client client1 = new Client();
+        });
+        client1Thread.start();
+
+        Thread client2Thread = new Thread(() -> {
+            Client client2 = new Client();
+        });
+        client2Thread.start();
     }
 }
